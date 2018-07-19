d6e3bbd368337eb839b96ed55e7a6da2053d1294
Shadman Kolahzary <info@kolahzary.ir>
2018-07-19 21:44:01 +0430

    Full request path on CatchAll

--- Sasuke.JSend/JSendCatchAllController.cs
+++ Sasuke.JSend/JSendCatchAllController.cs
@@ -5,7 +5,7 @@ namespace Sasuke.JSend
     public abstract class JSendCatchAllController : JSendControllerBase
     {
         protected string _NotFoundMessageFormat;
-        public JSendCatchAllController(string messageFormat = "There isn't any route definded for /{0}")
+        public JSendCatchAllController(string messageFormat = "There isn't any route definded for {0}")
         {
             _NotFoundMessageFormat = messageFormat;
         }
@@ -13,7 +13,7 @@ namespace Sasuke.JSend
         [Route("{*path}", Order = 999)]
         public ActionResult Get(string path)
         {
-            return JSendNotFound(string.Format(_NotFoundMessageFormat, path));
+            return JSendNotFound(string.Format(_NotFoundMessageFormat, Request.Path.Value));
         }
     }
 }
